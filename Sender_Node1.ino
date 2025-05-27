29717e944f3f4e0b8abfdae8f8505313a347bf75
Mohamed Asad Azim Bandarkar <111709895+mohamedasad10@users.noreply.github.com>
2025-05-27 23:30:48 +0200

    Node1 sender code
    Node 1 Sensors read temperature, light and humidity.

---
+++ Sender_Node1.ino
@@ -0,0 +1,135 @@
+// Node1
+// Two sensors are connected to the ESP32:
+// 1. Light sensor (e.g., LDR)
+// 2. Temperature & Humidity sensor (e.g., DHT11/DHT22)
+
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+#include "DFRobot_AHT20.h"
+
+DFRobot_AHT20 aht20;
+
+const int sensorPin = 36;
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 2
+
+// REPLACE WITH THE RECEIVER'S MAC Address
+uint8_t broadcastAddress[] = {0xa0, 0xb7, 0x65, 0x25, 0xd9, 0x68};
+
+// Structure example to send data
+// Must match the receiver structure#f8:b3:b7:2b:cf:d0
+typedef struct struct_message {
+  int id; 
+  float temperature;
+  float humidity;
+  int light;
+  int readingId;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// Create peer interface
+esp_now_peer_info_t peerInfo;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+
+unsigned int readingId = 0;
+
+// Insert your SSID
+constexpr char WIFI_SSID[] = "4x4V1";
+
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+      for (uint8_t i=0; i<n; i++) {
+          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+              return WiFi.channel(i);
+          }
+      }
+  }
+  return 0;
+}
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  uint8_t status;
+  while((status = aht20.begin()) != 0){
+    Serial.print("AHT20 sensor initialization failed. error status : ");
+    Serial.println(status);
+    delay(1000);
+  }
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+
+  WiFi.printDiag(Serial); // Uncomment to verify channel number before
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+  WiFi.printDiag(Serial); // Uncomment to verify channel change after
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+  }
+
+  myData.id = BOARD_ID;
+
+  // Set values to send
+  if(aht20.startMeasurementReady(/* crcEn = */true)){
+    myData.temperature = aht20.getTemperature_C();
+    myData.humidity = aht20.getHumidity_RH();
+  }
+
+  myData.light = analogRead(sensorPin);
+
+  myData.readingId = readingId++;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
